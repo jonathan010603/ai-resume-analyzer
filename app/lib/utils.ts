6decbde8f16f5817f0faa63256f3a22bbcbe9a5e
Jonathan <jonathan010603@gmail.com>
2026-01-21 13:13:58 -0300

    feat(Upload): form and files uploading
    Resume data form and file upload button

---
+++ app/lib/utils.ts
@@ -0,0 +1,16 @@
+/**
+ * Formats a file size in bytes to a human-readable string (KB, MB GB)
+ * @param bytes - The size in bytes
+ * @returns A formatted string with the appropriate unit
+ */
+
+export function formatSize(bytes: number): string {
+    if (bytes === 0) return '0 Bytes';
+
+    const k = 1024;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
+}
